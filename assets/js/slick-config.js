bccdd726464326518707473b54177148ca9405a4
gabrieleq <gabriel.evan.queiroz@gmail.com>
2017-05-18 04:16:18 -0300

    update and clean files

---
+++ assets/js/slick-config.js
@@ -0,0 +1,10 @@
+$('.slideshow').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 5000
+});
